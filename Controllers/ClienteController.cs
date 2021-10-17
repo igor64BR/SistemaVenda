b3697d548157d12a1ab0f913455f769c398f308c
igor64BR <igor64br@gmail.com>
2021-10-17 11:46:09 -0300

    V0.2 - Client Controller ready to use

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVenda.DAL;
+using SistemaVenda.Entidades;
+using SistemaVenda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Controllers
+{
+    public class ClienteController : Controller
+    {
+
+        protected ApplicationDbContext Db;
+
+        public ClienteController(ApplicationDbContext db)
+        {
+            Db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var Clientes = Db.Cliente.ToList();
+            Console.WriteLine(Clientes.Count());
+            return View(Clientes);
+        }
+        
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            var viewModel = new ClienteViewModel();
+
+            if (id != null)
+            {
+                var item = Db.Cliente.Where(x => x.Codigo == id).FirstOrDefault();
+                viewModel.Codigo = item.Codigo;
+                viewModel.Nome = item.Nome;
+                viewModel.CNPJ_CPF= item.CNPJ_CPF;
+                viewModel.Email= item.Email;
+                viewModel.Celular = item.Celular;
+
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(ClienteViewModel ViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var objCliente = new Cliente
+                {
+                    Codigo = ViewModel.Codigo,
+                    Nome = ViewModel.Nome,
+                    Celular = ViewModel.Celular,
+                    CNPJ_CPF = ViewModel.CNPJ_CPF,
+                    Email = ViewModel.Email,
+                };
+
+                if(objCliente.Codigo != null)
+                {
+                    Db.Entry(objCliente).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                }
+                else
+                {
+                    Db.Cliente.Add(objCliente);
+                }
+                Db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(ViewModel);
+            }
+        }
+
+        public IActionResult Excluir(int? id)
+        {
+            var objToDelete = Db.Cliente.Where(x => x.Codigo == id).FirstOrDefault();
+
+            Db.Cliente.Remove(objToDelete);
+            Db.SaveChanges();
+
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/Cliente.js
@@ -0,0 +1,23 @@
+﻿function GoTo(request) {
+    window.location = window.origin + "/Cliente/" + request
+}
+
+function CadastrarCliente() {
+    GoTo("Cadastro");
+}
+
+function Editar(id) {
+    GoTo("Cadastro/" + id)
+}
+
+function Redirect() {
+    GoTo("")
+}
+
+function Excluir(id) {
+    GoTo("Excluir/" + id)
+}
+
+function Novo() {
+    GoTo("Cadastro")
+}
